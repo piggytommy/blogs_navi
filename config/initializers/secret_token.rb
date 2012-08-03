0287e5f77e8c2f2a83a5438663ecfee35f41735d
qiuyuzhou <piggytommy@gmail.com>
2012-08-03 12:35:58 +0800

    project initilaized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogsNavi::Application.config.secret_token = '6007aac472e2af3caf7d6bde8e8a9091aad10290d161875322dc8bac20c594555bf2f17d525c184034a0a437140661868531ba18919391810b50207ff9d0506f'
